42a37110a70b4345d13f9fe6da2c943c2ace04f7
Chris Ohk <utilforever@gmail.com>
2022-10-08 19:40:06 +0900

    refactor: Change the return type of function 'main()' to 'Result'

--- src/main.rs
+++ src/main.rs
@@ -10,14 +10,14 @@ impl Drop for CleanUp {
     }
 }
 
-fn main() {
+fn main() -> crossterm::Result<()> {
     let _clean_up = CleanUp;
 
-    terminal::enable_raw_mode().expect("Could not turn on raw mode");
+    terminal::enable_raw_mode()?;
 
     loop {
-        if event::poll(Duration::from_millis(500)).expect("Error") {
-            if let Event::Key(event) = event::read().expect("Failed to read line") {
+        if event::poll(Duration::from_millis(500))? {
+            if let Event::Key(event) = event::read()? {
                 match event {
                     KeyEvent {
                         code: KeyCode::Char('q'),
@@ -33,4 +33,6 @@ fn main() {
             println!("No input yet\r");
         }
     }
+
+    Ok(())
 }
