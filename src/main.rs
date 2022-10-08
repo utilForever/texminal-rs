d19fdeebf8e012187a9cf673c7b0594d9ec6400c
Chris Ohk <utilforever@gmail.com>
2022-10-08 19:15:59 +0900

    feat: Add code to enable raw mode

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+use crossterm::terminal;
 use std::io::{self, Read};
 
 fn main() {
+    terminal::enable_raw_mode().expect("Could not turn on raw mode");
+
     let mut buf = [0; 1];
 
     while io::stdin().read(&mut buf).expect("Failed to read line") == 1 && buf != [b'q'] {}
