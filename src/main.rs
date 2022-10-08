5fcf2bfc7aec41ae1e44b456f0a220af0ade3de2
Chris Ohk <utilforever@gmail.com>
2022-10-08 19:13:00 +0900

    feat: Add code to process 'Press q To Quit'

--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,5 @@ use std::io::{self, Read};
 fn main() {
     let mut buf = [0; 1];
 
-    while io::stdin().read(&mut buf).expect("Failed to read line") == 1 {}
+    while io::stdin().read(&mut buf).expect("Failed to read line") == 1 && buf != [b'q'] {}
 }
