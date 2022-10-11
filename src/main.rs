75b500ef76aa0eb55c66e18449a20267c2bda18e
Chris Ohk <utilforever@gmail.com>
2022-10-11 23:40:18 +0900

    feat: Add code to clear screen on exit

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,7 @@ struct CleanUp;
 impl Drop for CleanUp {
     fn drop(&mut self) {
         terminal::disable_raw_mode().expect("Could not disable raw mode");
+        Output::clear_screen().expect("Error");
     }
 }
 
