ee0627f7e5941138c2993f0ddf5c768aaf33e123
Chris Ohk <utilforever@gmail.com>
2022-10-11 23:18:04 +0900

    refactor: Refactor code related to keyboard input

--- src/main.rs
+++ src/main.rs
@@ -10,29 +10,54 @@ impl Drop for CleanUp {
     }
 }
 
+struct Reader;
+
+impl Reader {
+    fn read_key(&self) -> crossterm::Result<KeyEvent> {
+        loop {
+            if event::poll(Duration::from_millis(500))? {
+                if let Event::Key(event) = event::read()? {
+                    return Ok(event);
+                }
+            }
+        }
+    }
+}
+
+struct Editor {
+    reader: Reader,
+}
+
+impl Editor {
+    fn new() -> Self {
+        Self { reader: Reader }
+    }
+
+    fn process_key_press(&self) -> crossterm::Result<bool> {
+        match self.reader.read_key()? {
+            KeyEvent {
+                code: KeyCode::Char('q'),
+                modifiers: event::KeyModifiers::CONTROL,
+                ..
+            } => return Ok(false),
+            _ => {}
+        }
+
+        Ok(true)
+    }
+
+    fn run(&self) -> crossterm::Result<bool> {
+        self.process_key_press()
+    }
+}
+
 fn main() -> crossterm::Result<()> {
     let _clean_up = CleanUp;
 
     terminal::enable_raw_mode()?;
 
-    loop {
-        if event::poll(Duration::from_millis(1000))? {
-            if let Event::Key(event) = event::read()? {
-                if let KeyEvent {
-                    code: KeyCode::Char('q'),
-                    modifiers: event::KeyModifiers::CONTROL,
-                    ..
-                } = event
-                {
-                    break;
-                }
-
-                println!("{:?}\r", event);
-            };
-        } else {
-            println!("No input yet\r");
-        }
-    }
+    let editor = Editor::new();
+    while editor.run()? {}
 
     Ok(())
 }
